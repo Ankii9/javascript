cf619c109393756a052b36e0827e94d77a92ec79
Ankit Raj <108692375+Ankii9@users.noreply.github.com>
2023-09-09 10:47:33 +0000

    dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 12345
+let accountEmail = "ankit@google.com"
+var accountPassword = "252322"
+accountCity = "Patna"  //not preferreble
+let accountState; //putting ";" is an option in js
+
+// accountId = 8 // not allowed
+
+accountEamil = "ar@ar.com"
+accountPassword = "8569"
+accountCity = "Bengaluru"
+
+console.log(accountId)
+
+/*
+    Prefer not use "var"
+    because of issues in block scope and functional scope
+*/
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState ])
